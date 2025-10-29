00fb4b80034617cfa8bc488fa4615f86121c0548
Luca Carniato <carniato.luca@gmail.com>
2025-10-29 16:33:15 +0100

    update

--- scratch.cpp
+++ scratch.cpp
@@ -133,4 +133,81 @@ for (TopTools_ListIteratorOfListOfShape it(Lsolids); it.More(); it.Next()) {
 
 
 
+// accumulators
+  BRep_Builder bb;
+  TopoDS_Compound cutOut;    bb.MakeCompound(cutOut);
+  TopoDS_Compound commonOut; bb.MakeCompound(commonOut);
+
+  for (TopTools_ListIteratorOfListOfShape it(Lparts); it.More(); it.Next()) {
+    const TopoDS_Shape& SL = it.Value();
+    TopTools_ListOfShape Rchunk = PickOverlapping(SL, rItems, rTree, /*halo*/ halo);
+
+    if (Rchunk.IsEmpty()) {
+      // nothing overlaps: whole left solid goes to the CUT result, no COMMON
+      bb.Add(cutOut, SL);
+      continue;
+    }
+
+    // --- build batch arguments
+    TopTools_ListOfShape args;  args.Append(SL);          // left
+    TopTools_ListOfShape tools;                           // right chunk
+    for (TopTools_ListIteratorOfListOfShape jr(Rchunk); jr.More(); jr.Next())
+      tools.Append(jr.Value());
+
+    // --- pave once
+    BOPAlgo_PaveFiller pf;
+    pf.SetNonDestructive(nonDestr);
+    pf.SetRunParallel(runParallel);
+    pf.SetUseOBB(useOBB);
+    pf.SetFuzzyValue(fuzzy);
+
+    TopTools_ListOfShape batchForFiller = args;
+    for (TopTools_ListIteratorOfListOfShape jt(tools); jt.More(); jt.Next())
+      batchForFiller.Append(jt.Value());
+    pf.SetArguments(batchForFiller);
+    pf.Perform();
+
+    // --- CUT: L \ R (reuse filler)
+    BOPAlgo_BOP opCut;
+    opCut.SetOperation(BOPAlgo_CUT);
+    opCut.SetNonDestructive(nonDestr);
+    opCut.SetRunParallel(runParallel);
+    opCut.SetUseOBB(useOBB);
+    opCut.SetFuzzyValue(fuzzy);
+    opCut.SetArguments(args);
+    opCut.SetTools(tools);
+    opCut.PerformWithFiller(pf);
+    bb.Add(cutOut, opCut.Shape());
+    opCut.Clear();
+
+    // --- COMMON: L ∩ R (reuse the same filler again)
+    BOPAlgo_BOP opCommon;
+    opCommon.SetOperation(BOPAlgo_COMMON);
+    opCommon.SetNonDestructive(nonDestr);
+    opCommon.SetRunParallel(runParallel);
+    opCommon.SetUseOBB(useOBB);
+    opCommon.SetFuzzyValue(fuzzy);
+    opCommon.SetArguments(args);
+    opCommon.SetTools(tools);
+    opCommon.PerformWithFiller(pf);
+    bb.Add(commonOut, opCommon.Shape());
+    opCommon.Clear();
+
+    // free pave DS before next batch
+    pf.Clear();
+  }
+
+  // optional cleanup to remove batch seams / duplicates
+  TopoDS_Shape cutRes    = cutOut;
+  TopoDS_Shape commonRes = commonOut;
+  if (unifyOut) {
+    ShapeUpgrade_UnifySameDomain uC(cutRes,    Standard_True, Standard_True, Standard_False);    uC.Build();    cutRes    = uC.Shape();
+    ShapeUpgrade_UnifySameDomain uI(commonRes, Standard_True, Standard_True, Standard_False);    uI.Build();    commonRes = uI.Shape();
+  }
+
+  return { cutRes, commonRes };
+}
+
+
+
 
